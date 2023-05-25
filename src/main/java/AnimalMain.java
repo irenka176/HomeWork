b8fc39ed320ea06f40cb7559b20dd6f5b68215d1
SEO\IKiselova <iryna.kiselova@onseo.biz>
2023-05-25 20:53:33 +0300

    DEV-12345 Lesson 3: 0.Figures

--- src/main/java/AnimalMain.java
+++ src/main/java/AnimalMain.java
@@ -1,10 +1,34 @@
 public class AnimalMain {
     public static void main(String[] args) {
+        /*
         Cat cat = new Cat();
         Dog dog = new Dog();
         cat.Swim(10);
         cat.Run(10);
         dog.Swim(150);
         dog.Run(200);
+        */
+
+        // create array of interfaces
+        InterfaceFigure  figures [] = new InterfaceFigure[3];
+
+        //added object to array by index
+        figures[0] = new Circle();
+        figures[1] = new Triangle();
+        figures[2] = new Square();
+
+        // start sum of all figures
+        double sAll = 0.0;
+
+        //cycle
+        for (int i = 0; i<figures.length; i++){
+            // get object with interface and call his method
+            double s = figures[i].getSquire();
+            // apply sum of figures
+            sAll = sAll + s;
+        }
+
+        System.out.println("area of figures: " + sAll);
+        }
     }
-}
+
---
+++ src/main/java/Triangle.java
@@ -0,0 +1,6 @@
+public class Triangle implements InterfaceFigure{
+    @Override
+    public double getSquire() {
+        return 21.5;
+    }
+}
