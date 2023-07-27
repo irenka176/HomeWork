a39e9c3915b46fb72664d814d462a5e5043f4cd6
SEO\IKiselova <iryna.kiselova@onseo.biz>
2023-07-27 22:20:28 +0300

    DEV-12345 Lesson 5: CollectionLesson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        String[][] inputArray = {
-                {"10", "12", "23", "14"},
-                {"65", "invalid value #1", "36", "17"},
-                {"invalid value #2", "55", "96", "67"},
-                {"51", "60", "invalid value #3", "87"}
-        };
-        int sum = 0;
-        try {
-            ArrayValueCalculator calculator = new ArrayValueCalculator();
-            sum = calculator.doCalc(inputArray);
-        } catch (ArraySizeException | ArrayDataException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Unknown exception: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            System.out.println("Sum of array elements: " + sum);
-        }
+        var collectionLesson = new CollectionLesson();
+        var response = collectionLesson.toList(new int[]{1, 2, 3, 4, 5, 6, 7, 7, 9, 10});
+        System.out.println("convert to list " + response);
+        var array = new String[]{"test", "test2", "test3", "test4", "test5", "test6", "test7", "test7", "test9", "test10",
+                "test", "test2", "test3", "test4", "test5", "test6", "test7", "test7"};
+        var request = collectionLesson.toList(array);
+        var count = collectionLesson.countOccurrence(request, "test100");
+        System.out.println("count test100 =  " + count);
+        count = collectionLesson.countOccurrence(request, "test");
+        System.out.println("count test = " + count);
+        count = collectionLesson.countOccurrence(request, "test7");
+        System.out.println("count test7 = " + count);
+        var unique = collectionLesson.findUnique(response);
+        System.out.println("unique values: " + unique);
     }
 }
